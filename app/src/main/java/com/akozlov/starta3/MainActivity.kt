55044b65ce9d2b4baeb5d96d002557d5b4bc7273
AlexK <a.privet.narnia@gmail.com>
2025-10-16 15:42:53 +0300

    Second 251016

--- app/src/main/java/com/akozlov/starta3/MainActivity.kt
+++ app/src/main/java/com/akozlov/starta3/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.akozlov.starta3.ui.theme.Starta3Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +22,11 @@ class MainActivity : ComponentActivity() {
         val state = mutableStateOf(0)
         enableEdgeToEdge()
         setContent {
+            Column(modifier = Modifier.padding(top = 40.dp)) {
             RecompositionScreen(state, onCounterClick = {state.value++})
+            InfoText(if (state.value<3) "More" else "Enough")
 
+            }
         }
     }
 }
--- app/src/main/java/com/akozlov/starta3/RecompositionScreen.kt
+++ app/src/main/java/com/akozlov/starta3/RecompositionScreen.kt
@@ -7,11 +7,14 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.State
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 
 @Composable
@@ -25,8 +28,13 @@ fun RecompositionScreen(counter: State<Int>, onCounterClick:() -> Unit) {
 fun ClickCounter(counter: State<Int>, onCounterClick: () -> Unit) {
     val counterValue = counter.value
     Log.d(TAG, "ClickCounter - $counterValue")
-    Column(modifier = Modifier.padding(top = 50.dp)){
-        Text(text = "Clicks: $counterValue", Modifier.clickable(onClick = onCounterClick))
 
-    }
+        Text(text = "Clicks: $counterValue", Modifier.clickable(onClick = onCounterClick).padding(top=50.dp), fontSize = 30.sp)
+
+}
+
+@Composable
+fun InfoText(text: String) {
+    Log.d(TAG, "InfoText")
+    Text(text = text, fontSize = 30.sp)
 }
