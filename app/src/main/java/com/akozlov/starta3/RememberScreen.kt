3e01a1ae7f8a014ee4466caeb12ecb616ce91684
AlexK <a.privet.narnia@gmail.com>
2025-10-17 17:03:13 +0300

    Third 251017

--- app/src/main/java/com/akozlov/starta3/RememberScreen.kt
+++ app/src/main/java/com/akozlov/starta3/RememberScreen.kt
@@ -1,19 +1,24 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
 package com.akozlov.starta3
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
+
 
 @Composable
 fun RememberScreen(counterValue: Int, onCounterClick: () -> Unit ) {
-    val evenOdd = EvenOdd()
+    val evenOdd = remember { EvenOdd() }
     Text(text = "Clicks: $counterValue  ${evenOdd.check(counterValue)}"
         , modifier = Modifier.clickable(onClick = onCounterClick), fontSize = 30.sp)
+
+    Log.d(TAG, "counterValue - $counterValue  evenOdd's hash - ${evenOdd.hashCode().toHexString()}")
 
 }
 
